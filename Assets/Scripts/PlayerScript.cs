0c71d3c9354455c8d4c4b7ef20164d3e4eaef816
BaneOfSmite <BaneOfSmite@gmail.com>
2019-07-21 19:25:11 +0800

    Added Gun Model and Gun Recoil

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,132 +2,119 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
-{
-    [Tooltip("Shooting rate of the player")]
-    public float ShootingRate;
+public class PlayerScript : MonoBehaviour {
+	[Tooltip("Shooting rate of the player")]
+	public float ShootingRate;
 
-    [Tooltip("Damage on enemy on each hit")]
-    public int ShootingDamage;
+	[Tooltip("Damage on enemy on each hit")]
+	public int ShootingDamage;
 
-    [Tooltip("Damage speed with contact with enemy")]
-    public int DamageRate;
+	[Tooltip("Damage speed with contact with enemy")]
+	public int DamageRate;
 
-    [Tooltip("Starting health of the enemy")]
-    public int HealthPoint;
+	[Tooltip("Starting health of the enemy")]
+	public int HealthPoint;
 
-    [Tooltip("Starting ammo of the enemy")]
-    public int AmmoCount;
+	[Tooltip("Starting ammo of the enemy")]
+	public int AmmoCount;
 
-    [Tooltip("Shooting sound effect")]
-    public AudioClip ShootingAudioClip;
+	[Tooltip("Shooting sound effect")]
+	public AudioClip ShootingAudioClip;
 
-    private Rigidbody rb = null;
-    private Vector3 moveDirection = Vector3.zero;
-    private bool canShoot;
-    private bool canDamage;
-    private AudioSource audioSource;
-    private GameObject camera = null;
+	private Rigidbody rb = null;
+	private Vector3 moveDirection = Vector3.zero;
+	private bool canShoot;
+	private bool canDamage;
+	private AudioSource audioSource;
+	private GameObject camera = null;
+	public Recoil RecoilObject;
 
-    // Use this for initialization
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
+	// Use this for initialization
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 
-        canShoot = true;
-        canDamage = true;
-        audioSource.clip = ShootingAudioClip;
-        camera = GameObject.FindGameObjectWithTag("MainCamera");
+		canShoot = true;
+		canDamage = true;
+		audioSource.clip = ShootingAudioClip;
+		camera = GameObject.FindGameObjectWithTag("MainCamera");
 
-        GameManager.Instance.UpdateAmmo(AmmoCount);
-        GameManager.Instance.UpdateHealth(HealthPoint);
-    }
+		GameManager.Instance.UpdateAmmo(AmmoCount);
+		GameManager.Instance.UpdateHealth(HealthPoint);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (GameManager.Instance.isGameOver)
-            return;
+	// Update is called once per frame
+	void Update() {
+		if (GameManager.Instance.isGameOver)
+			return;
 
-        Shoot();
-    }
+		Shoot();
+	}
 
-    private void Shoot()
-    {
-        if (Input.GetMouseButtonDown(0) && canShoot && AmmoCount > 0)
-        {
-            StartCoroutine(SpawnBullet());
-        }
-    }
+	private void Shoot() {
+		if (Input.GetMouseButtonDown(0) && canShoot && AmmoCount > 0) {
+			RecoilObject.recoil += 0.1f;
+			StartCoroutine(SpawnBullet());
+		}
+	}
 
-    private IEnumerator SpawnBullet()
-    {
-        RaycastHit hit;
+	private IEnumerator SpawnBullet() {
+		RaycastHit hit;
 
-        if (Physics.Raycast(camera.transform.position + camera.transform.forward, camera.transform.forward, out hit, Mathf.Infinity))
-        {
-            if (hit.collider.gameObject.tag.Equals("Enemy"))
-            {
-                hit.collider.gameObject.GetComponent<EnemyScript>().OnHit(ShootingDamage);
-            }
-        }
+		if (Physics.Raycast(camera.transform.position + camera.transform.forward, camera.transform.forward, out hit, Mathf.Infinity)) {
+			if (hit.collider.gameObject.tag.Equals("Enemy")) {
+				hit.collider.gameObject.GetComponent<EnemyScript>().OnHit(ShootingDamage);
+			}
+		}
 
-        audioSource.PlayOneShot(ShootingAudioClip);
+		audioSource.PlayOneShot(ShootingAudioClip);
 
-        GameManager.Instance.UpdateAmmo(--AmmoCount);
+		GameManager.Instance.UpdateAmmo(--AmmoCount);
 
-        canShoot = false;
-        //wait for some time
-        yield return new WaitForSeconds(ShootingRate);
+		canShoot = false;
+		//wait for some time
+		yield return new WaitForSeconds(ShootingRate);
 
-        canShoot = true;
-    }
+		canShoot = true;
+	}
 
-    private void OnTriggerStay(Collider collision)
-    {
-        if (collision.gameObject.tag.Equals("Enemy") && canDamage)
-        {
-            StartCoroutine(GetDamage(collision));
-        }
-    }
+	private void OnTriggerStay(Collider collision) {
+		if (collision.gameObject.tag.Equals("Enemy") && canDamage) {
+			StartCoroutine(GetDamage(collision));
+		}
+	}
 
-    private IEnumerator GetDamage(Collider collision)
-    {
-        EnemyScript enemyScript = collision.gameObject.GetComponent<EnemyScript>();
-        HealthPoint -= enemyScript.ContactDamage;
-        GameManager.Instance.UpdateHealth(HealthPoint);
+	private IEnumerator GetDamage(Collider collision) {
+		EnemyScript enemyScript = collision.gameObject.GetComponent<EnemyScript>();
+		HealthPoint -= enemyScript.ContactDamage;
+		GameManager.Instance.UpdateHealth(HealthPoint);
 
-        if (HealthPoint <= 0)
-        {
-            Dead();
-        }
+		if (HealthPoint <= 0) {
+			Dead();
+		}
 
-        canDamage = false;
-        //wait for some time
-        yield return new WaitForSeconds(DamageRate);
+		canDamage = false;
+		//wait for some time
+		yield return new WaitForSeconds(DamageRate);
 
-        canDamage = true;
-    }
+		canDamage = true;
+	}
 
-    private void Dead()
-    {
-        Destroy(gameObject);
-    }
+	private void Dead() {
+		Destroy(gameObject);
+	}
 
-    public void AddAmmo(int ammo, AudioClip audioClip)
-    {
-        AmmoCount += ammo;
-        GameManager.Instance.UpdateAmmo(AmmoCount);
+	public void AddAmmo(int ammo, AudioClip audioClip) {
+		AmmoCount += ammo;
+		GameManager.Instance.UpdateAmmo(AmmoCount);
 
-        audioSource.PlayOneShot(audioClip);
-    }
+		audioSource.PlayOneShot(audioClip);
+	}
 
-    public void AddHealth(int health, AudioClip audioClip)
-    {
-        HealthPoint += health;
-        GameManager.Instance.UpdateHealth(HealthPoint);
+	public void AddHealth(int health, AudioClip audioClip) {
+		HealthPoint += health;
+		GameManager.Instance.UpdateHealth(HealthPoint);
 
-        audioSource.PlayOneShot(audioClip);
-    }
+		audioSource.PlayOneShot(audioClip);
+	}
 }
