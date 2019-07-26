dddd2bfdc8f375a5b6d52412c7551f46829ec445
BaneOfSmite <BaneOfSmite@gmail.com>
2019-07-27 02:03:50 +0800

    Completed the Options Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,26 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject[] UIGameObjects;
+	public TMP_Dropdown _qualitysetting;
+	private int _quality;
+
 	void Start() {
-
-	}
-
-	// Update is called once per frame
-	void Update() {
+		if (PlayerPrefs.HasKey("Quality")) {
+			_quality = PlayerPrefs.GetInt("Quality");
+			_qualitysetting.value = _quality;
+			return;
+		}
+		_quality = QualitySettings.GetQualityLevel();
+		_qualitysetting.value = _quality;
 
 	}
 	public void Buttons(int ButtonType) {
 		switch (ButtonType) {
-			case 0:
+			case 0: //Quit
 				Application.Quit();
 				break;
-			case 1:
+			case 1: //Play
 				SceneManager.LoadScene(1);
+				break;
+			case 2: //Options
+				UIGameObjects[0].SetActive(false);
+				UIGameObjects[1].GetComponent<Animator>().SetBool("Options", true);
+				break;
+			case 3: //How To Play
+
+				break;
+			case 4: //Return To Menu From Options
+				UIGameObjects[1].GetComponent<Animator>().SetBool("Options", false);
+				StartCoroutine(DelayedFunction(1, 1.5f));
+				break;
+			case 5: //Return To Menu From How To Play
+
+				break;
+
+		}
+	}
+	public void QualityOption(int QualitySetting) {
+		QualitySettings.SetQualityLevel(QualitySetting);
+		PlayerPrefs.SetInt("Quality", QualitySetting);
+	}
+
+	private IEnumerator DelayedFunction(int type, float delay) {
+		yield return new WaitForSeconds(delay);
+		switch (type) {
+			case 1:
+				UIGameObjects[0].SetActive(true);
 				break;
 		}
 	}
---
+++ Assets/Scripts/VolumeAjust.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeAjust : MonoBehaviour {
+
+	public AudioMixer _AudioMixer;
+	public TextMeshProUGUI VolumeInt;
+	private float SavedVolume;
+
+	void Start() {
+		if (PlayerPrefs.HasKey("Audio" + transform.name)) {
+			SavedVolume = PlayerPrefs.GetFloat("Audio" + transform.name);
+			GetComponent<Slider>().value = SavedVolume;
+			_AudioMixer.SetFloat("Audio" + transform.name, Mathf.Log10(SavedVolume) * 20);
+			VolumeInt.text = Mathf.FloorToInt(SavedVolume * 100f).ToString();
+			return;
+		}
+		SavedVolume = 1;
+	}
+
+	public void SetVolume(float level) {
+		_AudioMixer.SetFloat("Audio" + transform.name, Mathf.Log10(level) * 20);
+		VolumeInt.text = Mathf.FloorToInt(level * 100f).ToString();
+		PlayerPrefs.SetFloat("Audio" + transform.name, level);
+	}
+}
